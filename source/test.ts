e28a23c8e77f640ceff206460889328e649e5fd5
Benjamin Lupton <b@lupton.cc>
2020-09-04 16:39:37 +1000

    changed var names in tests

--- source/test.ts
+++ source/test.ts
@@ -2,33 +2,33 @@ import { equal, gte } from 'assert-helpers'
 import kava from 'kava'
 import filedirname from './index.js'
 
-function verify(__filename: string, __dirname: string) {
-	console.log({ __filename, __dirname })
-	equal(typeof __filename, 'string', 'filename is a string')
-	equal(typeof __dirname, 'string', 'dirname is a string')
-	gte(__filename.length, 1, 'filename has a length')
-	gte(__dirname.length, 1, 'dirname has a length')
+function verify(file: string, dir: string) {
+	console.log({ file, dir })
+	equal(typeof file, 'string', 'file is a string')
+	equal(typeof dir, 'string', 'dir is a string')
+	gte(file.length, 1, 'file has a length')
+	gte(dir.length, 1, 'dir has a length')
 }
 
 kava.suite('filedirname', function (suite, test) {
 	test('caller', function () {
-		const [__filename, __dirname] = filedirname()
-		verify(__filename, __dirname)
+		const [file, dir] = filedirname()
+		verify(file, dir)
 	})
 	test('error', function () {
-		const [__filename, __dirname] = filedirname(new Error())
-		verify(__filename, __dirname)
+		const [file, dir] = filedirname(new Error())
+		verify(file, dir)
 	})
 	test('/dir/file.js', function () {
-		const [__filename, __dirname] = filedirname('/dir/file.js')
-		verify(__filename, __dirname)
+		const [file, dir] = filedirname('/dir/file.js')
+		verify(file, dir)
 	})
 	test('file:///dir/file.js', function () {
-		const [__filename, __dirname] = filedirname('file:///dir/file.js')
-		verify(__filename, __dirname)
+		const [file, dir] = filedirname('file:///dir/file.js')
+		verify(file, dir)
 	})
 	test('https://dir/file.js', function () {
-		const [__filename, __dirname] = filedirname('https://dir/file.js')
-		verify(__filename, __dirname)
+		const [file, dir] = filedirname('https://dir/file.js')
+		verify(file, dir)
 	})
 })
